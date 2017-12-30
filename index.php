d394e4528a7a2a4f434f8a21d7ba812b51eef03f
jatinder-pal <boskim.3ginfio@gmail.com>
2017-12-30 15:35:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $data = '';
 		}
 echo "hello";
 			$curl = curl_init("https://api.sandbox.paypal.com/v1/shipping/trackers");
-			$data = { "trackers": [
+			$data = array("trackers": [
 				{
 				"transaction_id"=>"123456789",
 				"tracking_number"=>"XYZ123456",
@@ -43,8 +43,8 @@ echo "hello";
 				"shipment_date"=>"2015-05-31",
 				"carrier"=>"SG_SG_POST"
 				}
-				]
-				};
+				]);
+				
 			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
 			curl_setopt($curl, CURLOPT_HEADER, false);
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
