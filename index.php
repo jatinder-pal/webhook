14bad0e0906c9254255484aec9e438b94c3a5915
jatinder-pal <boskim.3ginfio@gmail.com>
2018-01-02 15:35:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $data = '';
 	mail('boskim.3ginfo@gmail.com','testing',$order_id,"From: webmaster@example.com" . "\r\n");	
 	
 	}
-	 $order_id=72297906190;
+	 $order_id=238364262429;
 	$url='https://fd618d2f010bae1b72fc359c2e9ec5e6:058e8334fcd174ffa4ebdd761bf5e752@jai-shri-ram-2.myshopify.com/admin/orders/'.$order_id.'/transactions.json'; //rss link for the twitter timeline
 		$order_data = get_data($url); //dumps the content, you can manipulate as you wish to
 		//print_r($order_data);
