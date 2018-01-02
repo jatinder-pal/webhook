4faf447b86dddffab8d518cbaadbb724249f5ddd
jatinder-pal <boskim.3ginfio@gmail.com>
2018-01-02 13:08:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $data = '';
 		return $data;
 		}
 echo "hello";
-			$curl = curl_init("https://api.paypal.com/v1/shipping/trackers");
+			/*$curl = curl_init("https://api.paypal.com/v1/shipping/trackers");
 			 $data['trackers'] = array(
 				"transaction_id"=>"39X24218SL5770941",
 				"tracking_number"=>"XYZ123456",
@@ -55,5 +55,37 @@ echo "hello";
 			print_r($response );
 			if (!$response) {
 				die("Connection Failure.n");
-			}
+			}*/
+
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.paypal.com/v1/shipping/trackers/",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n\"trackers\": [\n{\n\"transaction_id\": \"39X24218SL5770941\",\n\"tracking_number\": \"XYZ123456\",\n\"status\": \"SHIPPED\",\n\"carrier\": \"SG_SG_POST\"\n}\n]\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer A21AAEQANA1o5GRhlkMh3IYyoRETfxHYoidhUivlXcL9ctEPVUAnl1IuM-YH6nqDGDR5DkxripUZYGo4DjO52tJ8zbX84z7QQ",
+    "Cache-Control: no-cache",
+    "Content-Type: application/json",
+    "Postman-Token: 4324ca06-c982-f4c6-6f7c-ff3a14c7fb10"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 	?>
