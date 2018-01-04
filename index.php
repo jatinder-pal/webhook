7627c386e5734f6af531836ddbd457b9e0217bd6
jatinder-pal <boskim.3ginfio@gmail.com>
2018-01-04 17:30:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -134,6 +134,57 @@ $data = '';
 				mail('boskim.3ginfo@gmail.com','testing',$order_id,"From: webmaster@example.com" . "\r\n");	
 		
 		}
-	  
+	  $order_id=72297906190;
+		$url='https://fd618d2f010bae1b72fc359c2e9ec5e6:058e8334fcd174ffa4ebdd761bf5e752@jai-shri-ram-2.myshopify.com/admin/orders/'.$order_id.'.json';
+		$order_data = get_data($url);
+		 $order_data=json_decode($order_data, true);
+		 echo "<pre>";print_r($order_data);echo "</pre>";
+		 $gateway=$data1['gateway']; 
+		echo $fulfillment_status = $order_data['order']['fulfillment_status']; 
+		echo $tracking_number=$order_data['order']['fulfillments'][0]['tracking_number']; 
+		echo $tracking_company='FEDEX'; 
+		if($fulfillment_status == 'fulfilled'){
+			$fulfillment_status = 'DELIVERED';
+		}
+		 echo $order_data['order']['gateway'];
+		function get_data($url)
+		{
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+		}
+		//$ch = curl_init("https://48889f0c2488fe101c19b98c2b12ad36:0b69dd28a3c9d7753bef022b939566e3@unmatched-market.myshopify.com/admin/orders/".$order_id."/.json");
+		$ch = curl_init("https://fd618d2f010bae1b72fc359c2e9ec5e6:058e8334fcd174ffa4ebdd761bf5e752@jai-shri-ram-2.myshopify.com/admin/orders/".$order_id.".json");
+	
+	$order = array(
+	"order" => array(
+		"note_attributes" => 
+			array(
+			"name"=>"gateway2",
+			"value"=> $gateway.'ful='.$fulfillment_status.'tracking='.$tracking_company.'tracking company='.$tracking_number
+			)
+		)
+	);
+		print_r($order);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($order)); 
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch); // close curl session
+		//print_r(json_decode($response, true));
+		$arr1=json_decode($response, true);
+		print_r($arr1);
+		$arr2=$arr1['order'];
+		if(count($response)>0){
+			echo'SUCCESS';
+		} else{
+			echo 'ERROR';
+	}
 		
 	?>
